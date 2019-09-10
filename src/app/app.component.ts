0b2d3f1b7a3e3ef6f3421cbba5cdc01cc1f1e32a
adriana cortes <adrianm-cortesv@unilibre.edu.co>
2019-09-10 12:23:33 -0500

    crear una inyeccion de depencia al controlador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {DocentesService} from './docentes.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DocentesService]
 })
 export class AppComponent {
-  title = 'solucitudlaboratorio';
+  title = 'solucitud-laboratorio';
+  lista = [];
+  constructor(servicio:DocentesService){
+    this.lista = servicio.getDocentes();
+  }
 }
